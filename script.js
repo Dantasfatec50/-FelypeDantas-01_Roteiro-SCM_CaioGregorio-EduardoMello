82a62a98a73003909e7a5f834f6f6221785c9406
Dantasfatec50 <z8022224@gmail.com>
2026-02-27 11:06:18 -0300

    8- parte 4 (integração do Aluno Eduardo Mello no grupo)

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
 
 // Aguarda o carregamento completo do DOM
 document.addEventListener('DOMContentLoaded', function() {
-    console.log('Landing Page carregada com sucessoooooooooooo!');
+    console.log('Landing Page carregada com sucessooooooooooooo!');
 });
 
 // Mensagem de boas-vindas no console
